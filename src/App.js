59084fd12667e702fd12b86468fadcf2c64f64c5
toth_szilvia <szylwy@gmail.com>
2022-04-07 16:15:13 +0200

    my first react import export virtual dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+const text = "Valami szöveg";
 
 function App() {
   return (
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}
         </p>
+        <p>Másik szöveg</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/MyApp.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyApp() {
+  return (
+    <div ClassName="MyApp">
+        <h1>This is my first react app</h1>
+        <p>Grat isn't it?</p>
+    </div>
+  );
+}
+
+export default MyApp;
